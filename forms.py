6d0e701bc5f2788f214cf10de84dcc5b088985b1
ctaylor4874 <ctaylor4874@gmail.com>
2016-12-18 16:38:23 -0600

    CamelCasedWord in the user-entered content automatically become links

--- forms.py
+++ forms.py
@@ -5,6 +5,7 @@ db = pg.DB(dbname='page_db')
 
 app = Flask('mywiki')
 
+
 # @app.route('/<page_name>/edit', methods=['POST'])
 # def update_form(page_name):
 #     title = page_name
@@ -31,19 +32,19 @@ def submit_new_page(page_name):
         page_content=page_content,
         last_modified_date=last_modified_date,
         author_last_modified=author_last_modified)
-# @app.route('/<page_name>')
-# def placeholder():
-#     page_title = request.query_string
-#     if not page_title:
-#         return redirect('/')
-#     # query = 'select id,page_title,page_content,last_modified_date,author_last_modified from page where name = %s' % name
-#     # cur.execute(query)
-#     # entry = cur.fetchone()
-#     # id = entry[0]
-#     # name = entry[1]
-#     # phone_number = entry[2]
-#     # email = entry[3]
-#     return render_template(
-#         'placeholder.html',
-#         page_title=page_title
-#     )
+    # @app.route('/<page_name>')
+    # def placeholder():
+    #     page_title = request.query_string
+    #     if not page_title:
+    #         return redirect('/')
+    #     # query = 'select id,page_title,page_content,last_modified_date,author_last_modified from page where name = %s' % name
+    #     # cur.execute(query)
+    #     # entry = cur.fetchone()
+    #     # id = entry[0]
+    #     # name = entry[1]
+    #     # phone_number = entry[2]
+    #     # email = entry[3]
+    #     return render_template(
+    #         'placeholder.html',
+    #         page_title=page_title
+    #     )
--- routes.py
+++ routes.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
+from wiki_linkify import wiki_linkify
 import mysql.connector
+from jinja2 import Environment, FileSystemLoader
 
 app = Flask('mywiki')
 
@@ -11,12 +13,21 @@ conn = mysql.connector.connect(
 
 cur = conn.cursor()
 
+# Environment.filters['wiki_linkify)'] = wiki_linkify
+
+# loader = FileSystemLoader('/tmp')
+env = Environment(loader=FileSystemLoader('templates'))
+env.filters['wiki_linkify'] = wiki_linkify
+view = env.get_template('view.html')
+
+
 @app.route("/")
 def home():
     query = "SELECT id,title FROM page"
     cur.execute(query)
     list = cur.fetchall()
     return render_template("wiki_home.html", title_list=list, title="Wiki Pages")
+
 
 @app.route('/<page_name>')
 def placeholder(page_name):
@@ -31,33 +42,32 @@ def placeholder(page_name):
         last_modified_date = entry[1]
         author_last_modified = entry[2]
 
-        return render_template(
-            'view.html',
+        return view.render(
             page_title=page_name,
             title=page_name,
             page_content=page_content,
             last_modified_date=last_modified_date,
             author_last_modified=author_last_modified)
     else:
-        return render_template('placeholder.html', title = page_name)
+        return render_template('placeholder.html', title=page_name)
+
 
 @app.route('/<page_name>/edit')
 def update_form(page_name):
     title = page_name
     if not title:
         return redirect('/')
-    sql = "SELECT COUNT(1) FROM page WHERE title = '%s'" %title
+    sql = "SELECT COUNT(1) FROM page WHERE title = '%s'" % title
     cur.execute(sql)
     if cur.fetchone()[0]:
         query = 'select page_content,last_modified_date,author_last_modified from page where title = %s' % title
         cur.execute(query)
         entry = cur.fetchone()
-        page_content = entry[1]
-        last_modified_date = entry[2]
-        author_last_modified = entry[3]
+        page_content = entry[0]
+        last_modified_date = entry[1]
+        author_last_modified = entry[2]
 
-        return render_template(
-            'view.html',
+        return view.render(
             page_title=page_name,
             title=page_name,
             page_content=page_content,
@@ -69,6 +79,8 @@ def update_form(page_name):
             page_title='Edit Page',
             title=page_name,
         )
+
+
 @app.route('/<page_name>/save', methods=['POST'])
 def submit_new_page(page_name):
     title = page_name
@@ -76,15 +88,17 @@ def submit_new_page(page_name):
     last_modified_date = request.form.get('last_modified_date')
     author_last_modified = request.form.get('author_last_modified')
     query = (
-    "insert into page (title, page_content, last_modified_date, author_last_modified) values(\"%s\", \"%s\", \"%s\", \"%s\")" % (title, page_content, last_modified_date, author_last_modified))
+        "insert into page (title, page_content, last_modified_date, author_last_modified) values(\"%s\", \"%s\", \"%s\", \"%s\")" % (
+            title, page_content, last_modified_date, author_last_modified))
     cur.execute(query)
     conn.commit()
     return render_template(
         "/view",
-        title = page_name,
-        page_content = page_content,
+        title=page_name,
+        page_content=page_content,
         last_modified_date=last_modified_date,
         author_last_modified=author_last_modified)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
